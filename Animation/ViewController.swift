24bb9d9a771f4705fef262e8ea23d7b8ac58a24f
Aizhan <106470967+zamkenova@users.noreply.github.com>
2022-10-18 15:08:49 +0600

    Initial Commit

---
+++ Animation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Animation
+//
+//  Created by Айжан Замкенова on 18.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
